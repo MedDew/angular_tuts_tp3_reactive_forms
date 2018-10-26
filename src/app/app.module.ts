44414c7b896bc62fbc6b9496ad70ed53e7e88bd1
Med <mehdi.gacem@versusmind.eu>
2018-10-26 16:36:23 +0200

    Import CarsModule to be used in the app view... + Import HttpClientModule to allow the use of HttpClient in CarCreateComponent + Provide BASE_ENDPOINT_CONFIG to allow its use in the car service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsModule } from './cars/cars.module';
+import { CarsComponent } from './cars/cars.component';
+// import { ReactiveFormsModule } from '@angular/forms';
+import { BASE_ENDPOINT_CONFIG } from './app.config';
+//Deprecated : DOES NOT WORK WITH rxjs/add/operator/map
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //CarsComponent,
+    // ReactiveFormsModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarsModule,
+    //HttpModule
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{provide : BASE_ENDPOINT_CONFIG, useValue : BASE_ENDPOINT_CONFIG.API_URL}],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule 
+{ 
+
+}
